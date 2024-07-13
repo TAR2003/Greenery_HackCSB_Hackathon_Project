014ee61492ff1c92ef16550f3fd74375827efc32
TAR2003 <108563700+TAR2003@users.noreply.github.com>
2024-07-13 22:39:52 +0600

    Cleared default layout

--- Nextjs/hackathon-project/app/page.js
+++ Nextjs/hackathon-project/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="">
       <div>Home will start from here </div>
+      <Link href={"/globalwarming"}>Click to go global warming</Link>
     </main>
   );
 }
