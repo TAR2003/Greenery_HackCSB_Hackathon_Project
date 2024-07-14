2c33cae4c01ecf9a84500536e43772f26378bd04
TAR2003 <108563700+TAR2003@users.noreply.github.com>
2024-07-14 21:21:46 +0600

    resolve route issure

--- Nextjs/hackathon-project/app/api/route.js
+++ Nextjs/hackathon-project/app/api/route.js
@@ -4,15 +4,15 @@
 
 //     return new Response(JSON.stringify(info));
 // }
-import { stringify } from "postcss";
-import { f } from "./login";
-export async function GET() {
-  return new Response("Hello weold" + (await f()));
-}
+// import { stringify } from "postcss";
+// import { f } from "./login";
+// export async function GET() {
+//   return new Response("Hello weold" + (await f()));
+// }
 
-export async function POST(request) {
-  const info = await request.json();
-  const type = info.type;
-  console.log(type);
-  return new Response(JSON.stringify("hhh"));
-}
+// export async function POST(request) {
+//   const info = await request.json();
+//   const type = info.type;
+//   console.log(type);
+//   return new Response(JSON.stringify("hhh"));
+// }
