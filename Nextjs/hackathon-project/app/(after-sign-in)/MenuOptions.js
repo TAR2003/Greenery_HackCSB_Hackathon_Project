2249583f07ebcbb4a329c10f1c6efe03b22132b9
TAR2003 <108563700+TAR2003@users.noreply.github.com>
2024-07-15 03:22:50 +0600

    Added the menu bar at the left

--- Nextjs/hackathon-project/app/(after-sign-in)/MenuOptions.js
+++ Nextjs/hackathon-project/app/(after-sign-in)/MenuOptions.js
@@ -1,8 +1,15 @@
 // MenuOptions.js
 const MenuOptions = [
-  { href: "/", label: "DOCS" },
-  { href: "/signin", label: "SIGN IN" },
-  { href: "/login", label: "LOG IN" },
+  { href: "/community", label: "Community" },
+  { href: "/forum", label: "Forum" },
+  { href: "/harvest", label: "Harvest" },
+  { href: "/plant-suggestion", label: "Plant Suggestion" },
+  { href: "/message", label: "Message" },
+  { href: "/marketplae", label: "Marketplace" },
+  { href: "/exchange", label: "Exchange" },
+  { href: "/greenery", label: "Greenery" },
+  { href: "/calender", label: "Plant Calender" },
+  { href: "/chat-ai", label: "Chat AI" },
 ];
 
 export default MenuOptions;
--- Nextjs/hackathon-project/app/(after-sign-in)/layout.js
+++ Nextjs/hackathon-project/app/(after-sign-in)/layout.js
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 
 const Layout = ({ children }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
   const router = useRouter();
 
   const toggleMenu = () => {
@@ -16,6 +17,10 @@ const Layout = ({ children }) => {
     setIsMenuOpen(false);
   };
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <>
       <header
@@ -23,13 +28,14 @@ const Layout = ({ children }) => {
         style={{ backgroundImage: "url('/green-green.png')" }}
       >
         <div className="flex items-center">
-          <div className=" pl-8">
+          <div className="pl-8">
+            {/* Adjust width and height of the Image component */}
             <button onClick={toggleMenu} className="focus:outline-none">
               <Image
                 src="/hamburger.png"
-                width={80}
-                height={80}
-                className="pl-0 hover:bg-yellow-500"
+                width={80} // Adjust width as needed
+                height={80} // Adjust height as needed
+                className="pl-0 hover:bg-gray-300"
                 alt="Menu"
               />
             </button>
@@ -47,16 +53,24 @@ const Layout = ({ children }) => {
 
         <div className="flex-1 text-right">
           <div className="flex flex-row gap-2 justify-end items-end pr-4 md:pr-20">
-            {MenuOptions.map((option, index) => (
-              <a
-                key={index}
-                href={option.href}
-                className="bg-blue-500 border border-white hover:bg-white text-white hover:text-black font-bold py-2 px-4 rounded w-24 md:w-32 h-10 text-center"
-                onClick={closeMenu}
-              >
-                {option.label}
-              </a>
-            ))}
+            <a
+              href="/"
+              className="bg-blue-500 border border-white hover:bg-white text-white hover:text-black font-bold py-2 px-4 rounded w-24 md:w-32 h-10 text-center"
+            >
+              DOCS
+            </a>
+            <a
+              href="/signin"
+              className="bg-blue-500 border border-white hover:bg-white text-white hover:text-black font-bold py-2 px-4 rounded w-24 md:w-32 h-10 text-center"
+            >
+              SIGN IN
+            </a>
+            <a
+              href="/login"
+              className="bg-blue-500 border border-white hover:bg-white text-white hover:text-black font-bold py-2 px-4 rounded w-24 md:w-32 h-10 text-center"
+            >
+              LOG IN
+            </a>
           </div>
         </div>
       </header>
@@ -66,18 +80,33 @@ const Layout = ({ children }) => {
         className={`fixed top-0 left-0 h-full bg-gray-800 text-white transition-transform transform ${
           isMenuOpen ? "translate-x-0" : "-translate-x-full"
         }`}
-        style={{ width: "250px" }}
+        style={{ width: "450px" }}
       >
-        <button onClick={toggleMenu} className="p-4 text-white">
-          Close
+        <button onClick={toggleMenu} className="pl-8 text-white">
+          <Image
+            src="/cross.png"
+            width={80}
+            height={80}
+            className="hover:bg-gray-300"
+            alt="Close"
+          />
         </button>
         <nav className="p-4">
           <ul>
+            <li className="py-2 pl-20 text-black">
+              <input
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={handleSearchChange}
+                className="bg-gray-200 px-4 py-2 rounded w-full hover:scale-110 transition-transform duration-300"
+              />
+            </li>
             {MenuOptions.map((option, index) => (
-              <li key={index} className="pl-20 hover:bg-blue-500">
+              <li key={index} className="py-2">
                 <a
                   href={option.href}
-                  className="text-white hover:text-yellow-500"
+                  className="block py-2 pl-20 hover:bg-blue-500 hover:scale-110 transform text-white hover:text-yellow-500 transition-transform duration-300"
                   onClick={closeMenu}
                 >
                   {option.label}
@@ -89,7 +118,7 @@ const Layout = ({ children }) => {
       </div>
 
       {/* Main content */}
-      <div className="py-20">{children}</div>
+      <div className="py-20 text-white">{children}</div>
 
       {/* Footer */}
       <footer>
