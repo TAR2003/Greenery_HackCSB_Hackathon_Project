2a560ae98e836d14c7f20f6a349bdcb17d53cd9c
TAR2003 <108563700+TAR2003@users.noreply.github.com>
2024-08-29 21:31:44 +0600

    menu fix

--- Nextjs/hackathon-project/app/(after-sign-in)/MenuOptions.js
+++ Nextjs/hackathon-project/app/(after-sign-in)/MenuOptions.js
@@ -1,17 +1,18 @@
 // MenuOptions.js
 const MenuOptions = [
   { href: "/community", label: "Community" },
-  { href: "/forum", label: "Forum" },
+
   { href: "/harvest", label: "Harvest" },
+  { href: "/forum", label: "Forum" },
+  { href: "/plants", label: "Plants" },
   { href: "/plant-exploration", label: "Plant Exploration" },
+
+  { href: "/plant-challenge", label: "Plant Challenges" },
+  { href: "/plant-journal", label: "Plant Journals" },
   { href: "/plant-detection", label: "Plant Identification" },
-  { href: "/message", label: "Message" },
+  { href: "/calender", label: "Plant Calender" },
   { href: "/marketplace", label: "Marketplace" },
   { href: "/chat-ai", label: "Chat AI" },
-  { href: "/plants", label: "Plants" },
-  { href: "/plant-challenge", label: "Plant Challenges" },
-  { href: "/plant-journal", label: "Plant Journal" },
-  { href: "/calender", label: "Plant Calender" },
 ];
 
 export default MenuOptions;
