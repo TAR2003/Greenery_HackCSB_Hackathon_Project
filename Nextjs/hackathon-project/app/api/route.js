7c8d1e1718ba4d71d49fd7bde5f50d9465690caf
Masnoon Muztahid <2005067@ugrad.cse.buet.ac.bd>
2024-08-30 16:44:08 +0600

    gemini model updated

--- Nextjs/hackathon-project/app/api/route.js
+++ Nextjs/hackathon-project/app/api/route.js
@@ -93,7 +93,7 @@ import {
 } from "./journalFunctions";
 
 const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 // Define the POST function
 export async function POST(request) {
@@ -615,6 +615,8 @@ export async function POST(request) {
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const text = await response.text();
+
+        
 
         return NextResponse.json({ text });
       } catch (error) {
