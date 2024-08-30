46130d55ad21721c8cafb09c6f0e2aa502444bf7
TAR2003 <108563700+TAR2003@users.noreply.github.com>
2024-08-30 17:55:53 +0600

    added comment n increase

--- Nextjs/hackathon-project/app/PostModal.js
+++ Nextjs/hackathon-project/app/PostModal.js
@@ -139,7 +139,7 @@ const PostModal = ({ elem, userinfo, isOpen, onClose, type }) => {
       );
     }
     setwriteComment("");
-    fetchData();
+    await fetchData();
   };
 
   useEffect(() => {
